b6ea6ddca1f2bd6516eb7156adad1b005d6dc743
Ryoma Hattori <rhattori0204@gmail.com>
2021-12-09 14:56:25 -0800

    minor

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 from distutils.core import setup
 import setuptools
 
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
 setup(
     name='dspca',
-    version='1.0.1',
+    version='1.0.2',
     packages=setuptools.find_packages(),
     url='http://github.com/ryhattori/dspca',
     license='MIT License',
     author='Ryoma Hattori',
     author_email='rhattori0204@gmail.com',
     description='Demixed subspace principal component analysis (dsPCA)',
+    long_description=readme,
+    long_description_content_type="text/markdown",
     install_requires=[
         "numpy",
         "scikit-learn",
